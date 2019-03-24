4a80e0e1fd160303e41bc0db329d6d30f1cd4e86
Nattanich Wisaiprom <nattanich15@hotmail.com>
2019-03-24 21:14:44 +0700

    Add bsp module

---
+++ TrafficLightController/MDK-ARM/bsp.c
@@ -0,0 +1,23 @@
+#include "bsp.h"
+
+// exported global variables	
+int Button_Status = 0 ;
+
+/** @brief Initialize I/0 for lamps
+    @retval None
+*/
+void BSP_Init(void);
+{
+}
+
+void Signal_Pass(void);
+{
+}
+
+void Signal_Block(void);
+{
+}
+
+void Signal_Flash(void);
+{
+}
